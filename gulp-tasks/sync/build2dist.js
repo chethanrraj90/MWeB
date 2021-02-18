67aa77b2b77f7405ace50b8147dc97bee664404a
Chethan . R <71137457+chethanrraj90@users.noreply.github.com>
2021-02-18 21:17:24 +0530

    Update build2dist.js

--- gulp-tasks/sync/build2dist.js
+++ gulp-tasks/sync/build2dist.js
@@ -1,7 +1,7 @@
 /*!
  * Project:     cv
  * File:        ./gulp-tasks/sync/build2dist.js
- * Copyright(c) 2016-nowdays Akhil Raj <akhilrajmailbox@gmail.com>
+ * Copyright(c) 2021-nowdays Chethan R <chethanrraj90@gmail.com>
  * License:     MIT
  */
 
