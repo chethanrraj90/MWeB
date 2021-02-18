4de94fb0e4dc16a31b20db26358fb737a8b9a45c
Chethan . R <71137457+chethanrraj90@users.noreply.github.com>
2021-02-18 21:21:27 +0530

    Update dist.js

--- gulp-tasks/clean/dist.js
+++ gulp-tasks/clean/dist.js
@@ -1,7 +1,7 @@
 /*!
  * Project:     cv
  * File:        ./gulp-tasks/clean/dist.js
- * Copyright(c) 2016-nowdays Akhil Raj <akhilrajmailbox@gmail.com>
+ * Copyright(c) 2021-nowdays Chethan R <chethanrraj90@gmail.com>
  * License:     MIT
  */
 
